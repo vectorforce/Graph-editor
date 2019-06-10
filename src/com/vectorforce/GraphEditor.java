09b56b4228651f24445f6ae75776df34983aa09a
Dima <vectorforce26@mail.ru>
2019-06-10 23:58:19 +0300

    Added and edited menuBar

--- src/com/vectorforce/GraphEditor.java
+++ src/com/vectorforce/GraphEditor.java
@@ -1,7 +1,10 @@
 package com.vectorforce;
 
-public class GraphEditor {
-    public static void main(String[] args){
+import com.vectorforce.View.MainWindow;
 
+public class GraphEditor {
+
+    public static void main(String[] args){
+        MainWindow mainWindow = new MainWindow();
     }
 }
---
+++ src/com/vectorforce/View/MainWindow.java
@@ -0,0 +1,71 @@
+package com.vectorforce.View;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
+
+public class MainWindow {
+    private Display display;
+    private Shell shell;
+
+    private void initMenuBar() {
+        Menu menuBar = new Menu(shell, SWT.BAR);
+        Menu fileMenu = new Menu(shell, SWT.DROP_DOWN);
+        Menu editMenu = new Menu(shell, SWT.DROP_DOWN);
+        MenuItem fileMenuItem = new MenuItem(menuBar, SWT.CASCADE);
+        fileMenuItem.setText("Файл");
+        fileMenuItem.setMenu(fileMenu);
+        MenuItem editMenuItem = new MenuItem(menuBar, SWT.CASCADE);
+        editMenuItem.setText("Редактировать");
+        editMenuItem.setMenu(editMenu);
+
+        MenuItem fileNewItem = new MenuItem(fileMenu, SWT.PUSH);
+        fileNewItem.setText("Новый");
+        MenuItem fileOpenItem = new MenuItem(fileMenu, SWT.PUSH);
+        fileOpenItem.setText("Открыть");
+        MenuItem fileCloseItem = new MenuItem(fileMenu, SWT.PUSH);
+        fileCloseItem.setText("Закрыть");
+        MenuItem fileSaveItem = new MenuItem(fileMenu, SWT.PUSH);
+        fileSaveItem.setText("Сохранить\tCtrl+S");
+        fileSaveItem.setAccelerator(SWT.CTRL + 'S');
+        MenuItem fileSaveAsItem = new MenuItem(fileMenu, SWT.PUSH);
+        fileSaveAsItem.setText("Сохранить как\tCtrl+Shift+S");
+        fileSaveAsItem.setAccelerator(SWT.CTRL + SWT.SHIFT + 'S');
+        MenuItem fileExitItem = new MenuItem(fileMenu, SWT.PUSH);
+        fileExitItem.setText("Выход\tCtrl+Q");
+        fileExitItem.setAccelerator(SWT.CTRL + 'Q');
+
+        // Initialization of listeners
+        fileExitItem.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                shell.close();
+            }
+        });
+
+        shell.setMenuBar(menuBar);
+    }
+
+    public MainWindow() {
+        display = new Display();
+        shell = new Shell(display);
+        shell.setText("Графовый редактор");
+        shell.setLayout(new GridLayout(1, true));
+
+        initMenuBar();
+
+        shell.open();
+
+        while (!shell.isDisposed()) {
+            if (display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+        display.dispose();
+    }
+}
